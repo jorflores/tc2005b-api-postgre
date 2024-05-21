a3ae7492cecdffd4ba4250cb6fdc41e6336065b9
Jorge Flores <flores.jorgeluis@gmail.com>
2024-05-21 17:48:05 -0600

    puerto a 4000

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-require('dotenv').config();
-const express = require('express');
-const sequelize = require('./config/database');
-const userRoutes = require('./routes/userRoutes');
-const cors = require("cors")
-const cookieParser = require('cookie-parser');
-
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./config/database");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
-
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
-    return sequelize.sync();  // Esta línea sincroniza los modelos con la base de datos.
+    console.log("Connection has been established successfully.");
+    return sequelize.sync(); // Esta línea sincroniza los modelos con la base de datos.
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
 
+app.use("/users", userRoutes);
 
-  app.use("/users", userRoutes)
-
-
-
-  app.listen(process.env.PORT || 5000, () => {
-    console.log('Server is running')
-  })
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Server is running");
+});
